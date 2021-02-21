ce40a6649b7fcd876646f892f03eeb8cfb1ebb84
Devon Mar <devon-mar@users.noreply.github.com>
2021-02-20 21:18:51 -0800

    Use reactions

--- constants.py
+++ constants.py
@@ -1,9 +1,8 @@
 NO_MESSAGES = "There are no messages for you to read right now."
 NO_REQUESTS = "You have no message requests."
 BAD_MESSAGE_REF = ":no_entry: Could not find the message you replied to."
-REPLY_RECEIVED = ":white_check_mark: Your reply has been received!"
-MESSAGE_REQUEST_RECEIVED = ":white_check_mark: Request Received!"
 INVALID_COMMAND = ":no_entry: Invalid command"
 GET_MSG_REQ_CMD = "get"
 RETRIEVE_MY_MSGS_CMD = "read"
 COMMAND_PREFIX = '/'
+SUCCESS_EMOJI = "✅"

4d49027e581ebb4efcd8d1217bc597472e76609b
devon-mar <devon-mar@users.noreply.github.com>
2021-02-20 22:26:19 -0800

    Check msg (#26)
    * stash
    
    * Add threshold for request deletion
    
    * Delete responses
    
    * Add check_message

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import jinja2
 
 
-async def render_template(template: str, **kwargs) -> str:
+def render_template(template: str, **kwargs) -> str:
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="templates/"), trim_blocks=True)
     template = env.get_template(template)
     return template.render(**kwargs)
+
+
+def check_message(msg: str) -> bool:
+    """
+    Returns True if the message is appropriate.
+    """
+    return "bad" not in msg

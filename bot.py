eea9a1c91f52a7b5851ac3e9bac1cb3e66331810
JoshFung <joshuagyfung@gmail.com>
2021-02-20 19:36:05 -0800

    Trying to come bine branch

--- bot.py
+++ bot.py
@@ -26,30 +26,25 @@ async def on_ready():
     bot.messages = []
     print(f"Logged in as {bot.user.name} {bot.user.id}")
 
-
-@bot.command(name="ping", brief="Replies pong", description="Literally just replies pong")
+@bot.command(name="ping")
 async def ping(ctx):
-    await ctx.channel.send("pong")
+    await ctx.reply("pong")
 
 
-@bot.command(name="respond", brief="Bot will send a message or request you can reply to",
-             description="The messages and requests that come from other users. You can reply "
-                         "to them through Discord's own reply feature by right clicking the message"
-                         ", then \"Reply\".")
+@bot.command(name="get")
 async def get_msg(ctx):
     req_count = session.query(MessageRequest).count()
     if req_count > 0:
         req = session.query(MessageRequest).filter(MessageRequest.user_id != ctx.author.id).first()
         if req is None:
-            await ctx.channel.send("There are no messages for you to read right now.")
+            await ctx.reply("There are no messages for you to read right now.")
         else:
             await req.send(ctx.channel, session)
     else:
-        await ctx.channel.send("No messages!")
+        await ctx.reply("No messages!")
 
 
-@bot.command(name="get_responses", brief="Bot sends you the responses sent to your message/request",
-             description="The bot will send back the responses to each of your messages/requests.")
+@bot.command(name="read")
 async def retrieve_my_msgs(ctx):
     my_mrs = session.query(MessageRequest).filter_by(user_id=ctx.author.id)
     if my_mrs.count() == 0:
@@ -61,25 +56,29 @@ async def retrieve_my_msgs(ctx):
         else:
             await ctx.channel.send(await render_template("read_replies.j2", req=req, mc=mc))
             req.delete(session)
-
-
 @bot.event
 async def on_message(message: discord.Message):
     if not isinstance(message.channel, discord.channel.DMChannel) or message.author.bot:
         return
-
     if message.reference is not None:
         rsp = session.query(Response).filter_by(discord_message=message.reference.message_id).first()
         if rsp is None:
             print(f"Ref not in DB: {message.reference.message_id}")
+            await message.reply(":no_entry: Could not find the message you replied to.")
         else:
             await rsp.set_message(message.content, session)
-            await message.channel.send("Your replied has been received!")
+            await message.reply(":white_check_mark: Your replied has been received!")
 
     elif message.content.startswith('/'):
         await bot.process_commands(message)
     else:
         await MessageRequest.create(message.content, message.author.id, session)
-        await message.channel.send("Received!")
+        await message.reply(":white_check_mark: Request Received!")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.reply(":no_entry: Invalid command")
 
 bot.run(DISCORD_TOKEN)
